ef9d0200dc2959c169f75df64b35bafbdb87d773
OliverSchlueter <oliverhd.1303@gmail.com>
2023-02-18 16:48:23 +0100

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    `java-library`
+    id("io.papermc.paperweight.userdev") version "1.5.1"
+    id("xyz.jpenilla.run-paper") version "2.0.1" // Adds runServer and runMojangMappedServer tasks for testing
+}
+
+group = "de.oliver"
+version = "1.0.0"
+description = "Hologram plugin"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+dependencies {
+    paperDevBundle("1.19.3-R0.1-SNAPSHOT")
+}
+
+tasks {
+    // Configure reobfJar to run when invoking the build task
+    assemble {
+        dependsOn(reobfJar)
+    }
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+
+        // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
+        // See https://openjdk.java.net/jeps/247 for more information.
+        options.release.set(17)
+    }
+    javadoc {
+        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+    }
+    processResources {
+        filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
+    }
+}
---
+++ src/main/java/de/oliver/FancyHolograms.java
@@ -0,0 +1,49 @@
+package de.oliver;
+
+import net.minecraft.server.dedicated.DedicatedServer;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_19_R2.CraftServer;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FancyHolograms extends JavaPlugin {
+
+    public static final String SUPPORTED_VERSION = "1.19.3";
+
+    private static FancyHolograms instance;
+
+    public FancyHolograms() {
+        instance = this;
+    }
+
+    @Override
+    public void onEnable() {
+        PluginManager pluginManager = Bukkit.getPluginManager();
+
+        DedicatedServer nmsServer = ((CraftServer) Bukkit.getServer()).getServer();
+
+        String serverVersion = nmsServer.getServerVersion();
+        if(!serverVersion.equals(SUPPORTED_VERSION)){
+            getLogger().warning("--------------------------------------------------");
+            getLogger().warning("Unsupported minecraft server version.");
+            getLogger().warning("Please update the server to " + SUPPORTED_VERSION + ".");
+            getLogger().warning("Disabling NPC plugin.");
+            getLogger().warning("--------------------------------------------------");
+            pluginManager.disablePlugin(this);
+            return;
+        }
+
+        String serverSoftware = nmsServer.getServerModName();
+        if(!serverSoftware.equals("Paper")){
+            getLogger().warning("--------------------------------------------------");
+            getLogger().warning("It is recommended to use Paper as server software.");
+            getLogger().warning("Because you are not using paper, the plugin");
+            getLogger().warning("might not work correctly.");
+            getLogger().warning("--------------------------------------------------");
+        }
+    }
+
+    public static FancyHolograms getInstance() {
+        return instance;
+    }
+}
