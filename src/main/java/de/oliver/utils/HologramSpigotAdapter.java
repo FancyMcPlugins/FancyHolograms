94674d82128374b9812e6f06f22f670977279e63
OliverSchlueter <oliverhd.1303@gmail.com>
2023-04-07 14:08:50 +0200

    Fix HologramSpigotAdapter

--- src/main/java/de/oliver/utils/HologramSpigotAdapter.java
+++ src/main/java/de/oliver/utils/HologramSpigotAdapter.java
@@ -7,54 +7,47 @@ import org.bukkit.craftbukkit.v1_19_R3.entity.CraftPlayer;
 import org.bukkit.entity.Display;
 import org.bukkit.entity.Player;
 
-public class HologramSpigotAdapter extends Hologram {
+public class HologramSpigotAdapter {
 
+    private final Hologram hologram;
     public HologramSpigotAdapter(Hologram hologram){
-        super(
-                hologram.getName(),
-                hologram.getLocation(),
-                hologram.getLines(),
-                hologram.getBillboard(),
-                hologram.getScale(),
-                hologram.getBackground(),
-                hologram.getUpdateTextInterval()
-        );
+        this.hologram = hologram;
     }
 
     public void spawn(Player serverPlayer) {
-        super.spawn(((CraftPlayer)serverPlayer).getHandle());
+        hologram.spawn(((CraftPlayer)serverPlayer).getHandle());
     }
 
     public void remove(Player player) {
-        super.remove(((CraftPlayer) player).getHandle());
+        hologram.remove(((CraftPlayer) player).getHandle());
     }
 
     public void updateText(Player player) {
-        super.updateText(((CraftPlayer) player).getHandle());
+        hologram.updateText(((CraftPlayer) player).getHandle());
     }
 
     public void updateLocation(Player player) {
-        super.updateLocation(((CraftPlayer) player).getHandle());
+        hologram.updateLocation(((CraftPlayer) player).getHandle());
     }
 
     public void updateBillboard(Player player) {
-        super.updateBillboard(((CraftPlayer) player).getHandle());
+        hologram.updateBillboard(((CraftPlayer) player).getHandle());
     }
 
     public void updateScale(Player player) {
-        super.updateScale(((CraftPlayer) player).getHandle());
+        hologram.updateScale(((CraftPlayer) player).getHandle());
     }
 
     public void updateBackground(Player player) {
-        super.updateBackground(((CraftPlayer) player).getHandle());
+        hologram.updateBackground(((CraftPlayer) player).getHandle());
     }
 
     public void setBillboard(Display.Billboard billboard) {
-        super.setBillboard(net.minecraft.world.entity.Display.BillboardConstraints.valueOf(billboard.name()));
+        hologram.setBillboard(net.minecraft.world.entity.Display.BillboardConstraints.valueOf(billboard.name()));
     }
 
     public void setBackground(ChatColor background) {
-        super.setBackground(ChatFormatting.getByCode(background.getChar()));
+        hologram.setBackground(ChatFormatting.getByCode(background.getChar()));
     }
 
     public static HologramSpigotAdapter fromHologram(Hologram hologram){
