33a57aeaedc54c6c41bc31d76c06324cfdd6b8f4
OliverSchlueter <oliverhd.1303@gmail.com>
2023-05-03 13:15:47 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,10 +22,10 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.FancyMcPlugins:FancyLib:7634814dfc")
-    compileOnly("de.oliver:FancyNpcs:1.1.3")
+    implementation("com.github.FancyMcPlugins:FancyLib:df4bc575eb")
+    compileOnly("de.oliver:FancyNpcs:1.1.4")
     compileOnly("me.clip:placeholderapi:2.11.3")
-    compileOnly("io.github.miniplaceholders:miniplaceholders-api:2.0.0")
+    compileOnly("io.github.miniplaceholders:miniplaceholders-api:2.1.0")
     paperweight.paperDevBundle("1.19.4-R0.1-SNAPSHOT")
 }
 
