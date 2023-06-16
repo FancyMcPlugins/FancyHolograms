3ceaed305a2e5d495d75baedf63821eba667d5c9
Sxtanna <ranald.taylor@gmail.com>
2023-06-16 03:35:08 -0400

    Feature: FancyHolograms Command Improvements (#17)
    * updated tab completion to
    - return an empty list instead of null
    - properly suggest sub-commands regardless of case
    
    * updated command executor to
    - return early for incorrect args length
    - properly return true/false for correct/incorrect usage
    - use completable future instead of a thread
    - use a text block instead of multiple messages
    
    * removed unused import
    
    * code cleanup (finalized class/fixed annotation/space)

--- src/main/java/de/oliver/fancyholograms/commands/FancyHologramsCMD.java
+++ src/main/java/de/oliver/fancyholograms/commands/FancyHologramsCMD.java
@@ -2,51 +2,72 @@ package de.oliver.fancyholograms.commands;
 
 import de.oliver.fancyholograms.FancyHolograms;
 import de.oliver.fancylib.MessageHelper;
-import de.oliver.fancynpcs.FancyNpcs;
 import org.apache.maven.artifact.versioning.ComparableVersion;
 import org.bukkit.command.*;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Stream;
 
-public class FancyHologramsCMD implements CommandExecutor, TabCompleter {
+public final class FancyHologramsCMD implements CommandExecutor, TabCompleter {
 
     @Override
-    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if(args.length == 1){
-            return Stream.of("version", "reload", "save").filter(input -> input.toLowerCase().startsWith(args[0])).toList();
+    public @NotNull List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (args.length != 1) {
+            return Collections.emptyList();
         }
-        return null;
+
+        return Stream.of("version", "reload", "save")
+                     .filter(alias -> alias.startsWith(args[0].toLowerCase(Locale.ROOT)))
+                     .toList();
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-
-        if(args.length == 1 && args[0].equalsIgnoreCase("version")){
-            MessageHelper.info(sender, "<i>Checking version, please wait...</i>");
-            new Thread(() -> {
-                ComparableVersion newestVersion = FancyHolograms.getInstance().getVersionFetcher().getNewestVersion();
-                ComparableVersion currentVersion = new ComparableVersion(FancyHolograms.getInstance().getDescription().getVersion());
-                if(newestVersion == null){
-                    MessageHelper.error(sender, "Could not find latest version");
-                } else if(newestVersion.compareTo(currentVersion) > 0){
-                    MessageHelper.warning(sender, "You are using an outdated version of the FancyHolograms Plugin");
-                    MessageHelper.warning(sender, "[!] Please download the newest version (" + newestVersion + "): <click:open_url:'" + FancyHolograms.getInstance().getVersionFetcher().getDownloadUrl() + "'><u>click here</u></click>");
-                } else {
-                    MessageHelper.success(sender, "You are using the latest version of the FancyHolograms Plugin (" + currentVersion + ")");
-                }
-            }).start();
-        } else if(args.length == 1 && args[0].equalsIgnoreCase("reload")){
-            FancyHolograms.getInstance().getFancyHologramsConfig().reload();
-            FancyHolograms.getInstance().getHologramManager().reloadHolograms();
-            MessageHelper.success(sender, "Reloaded config and holograms");
-        } else if(args.length == 1 && args[0].equalsIgnoreCase("save")){
-            FancyHolograms.getInstance().getHologramManager().saveHolograms(true);
-            MessageHelper.success(sender, "Saved all holograms");
+        if (args.length != 1) {
+            return false;
         }
 
-        return false;
+        final var plugin = FancyHolograms.getInstance();
+
+        switch (args[0].toLowerCase(Locale.ROOT)) {
+            case "save" -> {
+                plugin.getHologramManager().saveHolograms(true);
+                MessageHelper.success(sender, "Saved all holograms");
+            }
+            case "reload" -> {
+                plugin.getFancyHologramsConfig().reload();
+                plugin.getHologramManager().reloadHolograms();
+                MessageHelper.success(sender, "Reloaded config and holograms");
+            }
+            case "version" -> {
+                MessageHelper.info(sender, "<i>Checking version, please wait...</i>");
+
+                final var fetcher = plugin.getVersionFetcher();
+                final var current = new ComparableVersion(plugin.getDescription().getVersion());
+
+                CompletableFuture.supplyAsync(fetcher::getNewestVersion).whenComplete((newest, error) -> {
+                    if (newest == null || error != null) {
+                        MessageHelper.error(sender, "Could not find latest version");
+                    } else if (newest.compareTo(current) > 0) {
+                        MessageHelper.warning(sender, """
+                                                      You are using an outdated version of the FancyHolograms Plugin
+                                                      [!] Please download the newest version (%s): <click:open_url:'%s'><u>click here</u></click>
+                                                      """.formatted(newest, fetcher.getDownloadUrl()));
+                    } else {
+                        MessageHelper.success(sender, "You are using the latest version of the FancyHolograms Plugin (" + current + ")");
+                    }
+                });
+            }
+            default -> {
+                return false;
+            }
+        }
+
+        return true;
     }
+
 }
