301188729137293ad4c91281a9f94d8e37dda7f1
OliverSchlueter <OliverSchlueter@users.noreply.github.com>
2023-10-19 20:58:50 +0200

    Update version to 2.0.3-alpha3 (maven is stupid)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ val minecraftVersion = "1.20.2"
 
 allprojects {
     group = "de.oliver"
-    version = "2.0.3-alpha1"
+    version = "2.0.3-alpha2"
 
     description = "Simple, lightweight and fast hologram plugin using display entities"
 
