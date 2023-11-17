5e8d94d9dbdfdf4e5b25e1233e771f1012af71f6
OliverSchlueter <OliverSchlueter@users.noreply.github.com>
2023-11-17 19:08:20 +0100

    Update version to 2.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ val minecraftVersion = "1.20.2"
 
 allprojects {
     group = "de.oliver"
-    version = "2.0.3-types"
+    version = "2.0.3"
 
     description = "Simple, lightweight and fast hologram plugin using display entities"
 
