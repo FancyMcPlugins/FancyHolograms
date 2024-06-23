1388a3fd32f2d91d72c47054406524d4dab1fe79
Oak <cooldaveb@live.co.uk>
2024-06-23 21:51:06 +0100

    Added basic manual visibility implementation

--- api/src/main/java/de/oliver/fancyholograms/api/data/property/Visibility.java
+++ api/src/main/java/de/oliver/fancyholograms/api/data/property/Visibility.java
@@ -1,10 +1,12 @@
 package de.oliver.fancyholograms.api.data.property;
 
+import com.google.common.collect.HashMultimap;
 import de.oliver.fancyholograms.api.hologram.Hologram;
 import org.bukkit.entity.Player;
 
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.UUID;
 
 public enum Visibility {
     /**
@@ -15,12 +17,14 @@ public enum Visibility {
      * The player needs permission to see a specific hologram.
      */
     PERMISSION_REQUIRED(
-            (player, hologram) -> player.hasPermission("fancyholograms.viewhologram." + hologram.getData().getName())
-    );
-
+        (player, hologram) -> player.hasPermission("fancyholograms.viewhologram." + hologram.getData().getName())
+    ),
+    /**
+     * The player needs to be added manually through the API
+     */
+    MANUAL(ManualVisibility::canSee);
 
     private final VisibilityPredicate predicate;
-
 
     Visibility(VisibilityPredicate predicate) {
         this.predicate = predicate;
@@ -28,8 +32,8 @@ public enum Visibility {
 
     public static Optional<Visibility> byString(String value) {
         return Arrays.stream(Visibility.values())
-                .filter(visibility -> visibility.toString().equalsIgnoreCase(value))
-                .findFirst();
+            .filter(visibility -> visibility.toString().equalsIgnoreCase(value))
+            .findFirst();
     }
 
     public boolean canSee(Player player, Hologram hologram) {
@@ -39,5 +43,44 @@ public enum Visibility {
     public interface VisibilityPredicate {
 
         boolean canSee(Player player, Hologram hologram);
+    }
+
+    /**
+     * Handling of Visibility.MANUAL
+     */
+    public static class ManualVisibility {
+        private static final HashMultimap<String, UUID> distantViewers = HashMultimap.create();
+
+        public static boolean canSee(Player player, Hologram hologram) {
+            return hologram.isViewer(player) || distantViewers.containsEntry(hologram.getName(), player.getUniqueId());
+        }
+
+        public static void addDistantViewer(Hologram hologram, UUID uuid) {
+            addDistantViewer(hologram, uuid);
+        }
+
+        public static void addDistantViewer(String hologramName, UUID uuid) {
+            distantViewers.put(hologramName, uuid);
+        }
+
+        public static void removeDistantViewer(Hologram hologram, UUID uuid) {
+            removeDistantViewer(hologram, uuid);
+        }
+
+        public static void removeDistantViewer(String hologramName, UUID uuid) {
+            distantViewers.remove(hologramName, uuid);
+        }
+
+        public static void remove(Hologram hologram) {
+            remove(hologram.getName());
+        }
+
+        public static void remove(String hologramName) {
+            distantViewers.removeAll(hologramName);
+        }
+
+        public static void clear() {
+            distantViewers.clear();
+        }
     }
 }
