77bdfc4a9cf1389bdf78e7b122414c73d78d4e60
danny <danny@atropos.cc>
2025-03-06 06:38:10 -0500

    Refactor to make this less messy

--- src/main/java/de/oliver/fancyholograms/FancyHologramsConfiguration.java
+++ src/main/java/de/oliver/fancyholograms/FancyHologramsConfiguration.java
@@ -3,6 +3,7 @@ package de.oliver.fancyholograms;
 import de.oliver.fancyholograms.api.FancyHologramsPlugin;
 import de.oliver.fancyholograms.api.HologramConfiguration;
 import de.oliver.fancylib.ConfigHelper;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
@@ -33,7 +34,6 @@ public final class FancyHologramsConfiguration implements HologramConfiguration 
      */
     private boolean saveOnChangedEnabled;
     private static final String CONFIG_SAVE_ON_CHANGED = "saving.save_on_changed";
-
 
     /**
      * The log level for the plugin.
@@ -70,98 +70,94 @@ public final class FancyHologramsConfiguration implements HologramConfiguration 
     private static final String CONFIG_REPORT_ERRORS_TO_SENTRY = "logging.report_errors_to_sentry";
     private static final String CONFIG_VERSION = "config_version";
 
-    @Override
-    public void reload(@NotNull FancyHologramsPlugin plugin) {
-        FancyHolograms pluginImpl = (FancyHolograms) plugin;
-        pluginImpl.reloadConfig();
+    private static final Map<String, List<String>> CONFIG_COMMENTS = Map.of(
+            CONFIG_VERSION, List.of("Config version, do not modify."),
+            CONFIG_AUTOSAVE_ENABLED, List.of("Whether autosave is enabled."),
+            CONFIG_AUTOSAVE_INTERVAL, List.of("The interval at which autosave is performed in minutes."),
+            CONFIG_SAVE_ON_CHANGED, List.of("Whether the plugin should save holograms when they are changed."),
+            CONFIG_LOG_LEVEL, List.of("The log level for the plugin (DEBUG, INFO, WARN, ERROR)."),
+            CONFIG_LOG_ON_WORLD_LOAD, List.of("Whether hologram loading should be logged on world loading. Disable this if you load worlds dynamically to prevent console spam."),
+            CONFIG_VERSION_NOTIFICATIONS, List.of("Whether the plugin should send notifications for new updates."),
+            CONFIG_VISIBILITY_DISTANCE, List.of("The default visibility distance for holograms."),
+            CONFIG_REGISTER_COMMANDS, List.of("Whether the plugin should register its commands.")
+    );
 
-        final var config = pluginImpl.getConfig();
-
+    private void updateChecker(@NotNull FancyHolograms plugin, @NotNull FileConfiguration config) {
         final int latestVersion = 1;
         int configVersion = (int) ConfigHelper.getOrDefault(config, CONFIG_VERSION, 0);
 
-        if (configVersion < latestVersion ) {
-
-            try {
-                pluginImpl.getFancyLogger().warn("Outdated config detected! Attempting to migrate previous settings to new config...");
-
-                var oldConfig = pluginImpl.getConfig();
-                File backupFile = new File(pluginImpl.getDataFolder(), "config_old.yml");
-                if (backupFile.exists() && !backupFile.canWrite()) {
-                    throw new IOException("Unable to backup config to " + backupFile.getPath());
-                }
-                oldConfig.save(backupFile);
-
-                pluginImpl.saveDefaultConfig();
-                var newConfig = pluginImpl.getConfig();
-
-                Map<String, Object> oldConfigValues = oldConfig.getValues(true);
-                oldConfigValues.forEach((key, value) -> {
-
-                    Map<String, String> keyMap = Map.of(
-                            "enable_autosave", CONFIG_AUTOSAVE_ENABLED,
-                            "autosave_interval", CONFIG_AUTOSAVE_INTERVAL,
-                            "save_on_changed", CONFIG_SAVE_ON_CHANGED,
-                            "log_level", CONFIG_LOG_LEVEL,
-                            "mute_version_notifications", CONFIG_VERSION_NOTIFICATIONS
-                    );
-
-                    String newKey = keyMap.getOrDefault(key, null);
-                    if (newKey != null) {
-                        if (newKey.equals(CONFIG_VERSION_NOTIFICATIONS)) {
-                            newConfig.set(newKey, !(Boolean) value);
-                        } else {
-                            newConfig.set(newKey, value);
-                        }
-                    } else if (newConfig.contains(key)) {
-                        newConfig.set(key, value);
-                    } else {
-                        pluginImpl.getFancyLogger().warn("> CONFIG: Option '" + key + "' is deprecated/invalid! Please migrate this manually from config_old.yml");
-                        return;
-                    }
-
-                    pluginImpl.getFancyLogger().info("> CONFIG: Set option '" + key + "' to '" + value + "' from old config.");
-                });
-
-                newConfig.set(CONFIG_VERSION, latestVersion);
-                pluginImpl.getFancyLogger().info("Configuration has finished migrating. Please double check your settings in config.yml.");
-
-            } catch (IOException e) {
-                pluginImpl.getFancyLogger().error("Failed to save or reload configuration: " + e.getMessage());
-            }
+        if (configVersion >= latestVersion ) {
+            setOptions(config);
+            return;
         }
+            plugin.getFancyLogger().warn("Outdated config detected! Attempting to migrate previous settings to new config...");
 
-        config.setInlineComments(CONFIG_VERSION, List.of("Config version, do not modify."));
+            var oldConfig = plugin.getConfig();
+            try {
+                File backupFile = new File(plugin.getDataFolder(), "config_old.yml");
+                oldConfig.save(backupFile);
+            } catch (IOException e) {
+                plugin.getFancyLogger().warn("Unable to backup config to config_old.yml:" + e);
+            }
+
+
+            var newConfig = plugin.getConfig();
+
+            Map<String, Object> oldConfigValues = oldConfig.getValues(true);
+            Map<String, String> keyMap = Map.of(
+                    "enable_autosave", CONFIG_AUTOSAVE_ENABLED,
+                    "autosave_interval", CONFIG_AUTOSAVE_INTERVAL,
+                    "save_on_changed", CONFIG_SAVE_ON_CHANGED,
+                    "log_level", CONFIG_LOG_LEVEL,
+                    "mute_version_notifications", CONFIG_VERSION_NOTIFICATIONS
+            );
+
+            oldConfigValues.forEach((key, value) -> {
+
+                String newKey = keyMap.getOrDefault(key, null);
+                if (newKey != null) {
+                    if (newKey.equals(CONFIG_VERSION_NOTIFICATIONS)) {
+                        newConfig.set(newKey, !(Boolean) value);
+                    } else {
+                        newConfig.set(newKey, value);
+                    }
+                    plugin.getFancyLogger().info("> CONFIG: Set option '" + key + "' to '" + value + "' from old config.");
+                } else {
+                    plugin.getFancyLogger().warn("> CONFIG: Option '" + key + "' is deprecated/invalid! Please migrate this manually from config_old.yml");
+                }
+            });
+
+            newConfig.set(CONFIG_VERSION, latestVersion);
+            setOptions(newConfig);
+            CONFIG_COMMENTS.forEach(config::setInlineComments);
+
+            plugin.getFancyLogger().info("Configuration has finished migrating. Please double check your settings in config.yml.");
+    }
+
+    private void setOptions(@NotNull FileConfiguration config) {
 
         // saving
         autosaveEnabled = (boolean) ConfigHelper.getOrDefault(config, CONFIG_AUTOSAVE_ENABLED, true);
-        config.setInlineComments(CONFIG_AUTOSAVE_ENABLED, List.of("Whether autosave is enabled."));
-
         autosaveInterval = (int) ConfigHelper.getOrDefault(config, CONFIG_AUTOSAVE_INTERVAL, 15);
-        config.setInlineComments(CONFIG_AUTOSAVE_INTERVAL, List.of("The interval at which autosave is performed in minutes."));
-
         saveOnChangedEnabled = (boolean) ConfigHelper.getOrDefault(config, CONFIG_SAVE_ON_CHANGED, true);
-        config.setInlineComments(CONFIG_SAVE_ON_CHANGED, List.of("Whether the plugin should save holograms when they are changed."));
-
         // logging
         logLevel = (String) ConfigHelper.getOrDefault(config, CONFIG_LOG_LEVEL, "INFO");
-        config.setInlineComments(CONFIG_LOG_LEVEL, List.of("The log level for the plugin (DEBUG, INFO, WARN, ERROR)."));
-
         hologramLoadLogging = (boolean) ConfigHelper.getOrDefault(config, CONFIG_LOG_ON_WORLD_LOAD, true);
-        config.setInlineComments(CONFIG_LOG_ON_WORLD_LOAD, List.of("Whether hologram loading should be logged on world loading. Disable this if you load worlds dynamically to prevent console spam."));
-
         versionNotifs = (boolean) ConfigHelper.getOrDefault(config, CONFIG_VERSION_NOTIFICATIONS, true);
-        config.setInlineComments(CONFIG_VERSION_NOTIFICATIONS, List.of("Whether the plugin should send notifications for new updates."));
-
-        config.set(CONFIG_REPORT_ERRORS_TO_SENTRY, null);
-        config.setInlineComments(CONFIG_REPORT_ERRORS_TO_SENTRY, List.of());
-
         // options
         defaultVisibilityDistance = (int) ConfigHelper.getOrDefault(config, CONFIG_VISIBILITY_DISTANCE, 20);
-        config.setInlineComments(CONFIG_VISIBILITY_DISTANCE, List.of("The default visibility distance for holograms."));
-
         registerCommands = (boolean) ConfigHelper.getOrDefault(config, CONFIG_REGISTER_COMMANDS, true);
-        config.setInlineComments(CONFIG_REGISTER_COMMANDS, List.of("Whether the plugin should register its commands."));
+
+        config.set(CONFIG_REPORT_ERRORS_TO_SENTRY, null);
+    }
+
+    @Override
+    public synchronized void reload(@NotNull FancyHologramsPlugin plugin) {
+        FancyHolograms pluginImpl = (FancyHolograms) plugin;
+        pluginImpl.reloadConfig();
+
+        var config = pluginImpl.getConfig();
+        updateChecker(pluginImpl, config);
 
         if (pluginImpl.isEnabled() && !plugin.getHologramThread().isShutdown()) {
             plugin.getHologramThread().submit(pluginImpl::saveConfig);
